31affd63c8dc82a007573179152c7e82b705ddc9
Cecilia Barasa <cecibarasa@gmail.com>
2020-05-29 13:40:08 +0300

    add 404 error handler

---
+++ app/main/errors.py
@@ -0,0 +1,10 @@
+from flask import render_template
+from . import main
+
+@main.app_errorhandler(404)
+def four_Ow_four(error):
+    '''
+    Method to render the error page
+    '''
+
+    return render_template('fourOwfour.html'),404
